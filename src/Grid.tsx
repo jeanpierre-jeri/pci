75548d4ad4c736a838f292c71286fd60780920fa
Jean Pierre <jeanpierre.webdev@gmail.com>
2022-12-15 11:25:11 -0500

    Task #2 - Added sorting and filtering to the grid

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -4,23 +4,27 @@ import data from './near-earth-asteroids.json'
 import 'ag-grid-community/styles/ag-grid.css'
 import 'ag-grid-community/styles/ag-theme-alpine.css'
 
+const defaultColDef = {
+  sortable: true
+}
+
 const columnDefs: ColDef[] = [
-  { field: 'designation', headerName: 'Designation' },
-  { field: 'discovery_date', headerName: 'Discovery Date' },
-  { field: 'h_mag', headerName: 'H (mag)' },
-  { field: 'moid_au', headerName: 'MOID (au)' },
-  { field: 'q_au_1', headerName: 'q (au)' },
-  { field: 'q_au_2', headerName: 'Q (au)' },
-  { field: 'period_yr', headerName: 'Period (yr)' },
-  { field: 'i_deg', headerName: 'Inclination (deg)' },
-  { field: 'pha', headerName: 'Potentially Hazardous' },
-  { field: 'orbit_class', headerName: 'Orbit Class', enableRowGroup: true }
+  { field: 'designation', headerName: 'Designation', filter: 'agTextColumnFilter' },
+  { field: 'discovery_date', headerName: 'Discovery Date', filter: 'agDateColumnFilter' },
+  { field: 'h_mag', headerName: 'H (mag)', filter: 'agNumberColumnFilter' },
+  { field: 'moid_au', headerName: 'MOID (au)', filter: 'agNumberColumnFilter' },
+  { field: 'q_au_1', headerName: 'q (au)', filter: 'agNumberColumnFilter' },
+  { field: 'q_au_2', headerName: 'Q (au)', filter: 'agNumberColumnFilter' },
+  { field: 'period_yr', headerName: 'Period (yr)', filter: 'agNumberColumnFilter' },
+  { field: 'i_deg', headerName: 'Inclination (deg)', filter: 'agNumberColumnFilter' },
+  { field: 'pha', headerName: 'Potentially Hazardous', filter: 'agTextColumnFilter' },
+  { field: 'orbit_class', headerName: 'Orbit Class', enableRowGroup: true, filter: 'agTextColumnFilter' }
 ]
 
 const NeoGrid = (): JSX.Element => {
   return (
     <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
-      <AgGridReact rowData={data} columnDefs={columnDefs} rowGroupPanelShow={'always'} />
+      <AgGridReact defaultColDef={defaultColDef} rowData={data} columnDefs={columnDefs} rowGroupPanelShow={'always'} />
     </div>
   )
 }
